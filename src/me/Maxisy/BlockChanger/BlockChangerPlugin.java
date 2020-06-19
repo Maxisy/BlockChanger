95e6bea13228f9d66c4c2c7280bfe379b872fcdf
Maxisy <maks.sybicki@gmail.com>
2020-06-19 13:31:24 +0200

    added reload config command

--- src/me/Maxisy/BlockChanger/BlockChangerPlugin.java
+++ src/me/Maxisy/BlockChanger/BlockChangerPlugin.java
@@ -3,10 +3,14 @@ package me.Maxisy.BlockChanger;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public class BlockChangerPlugin extends JavaPlugin {
 
@@ -15,20 +19,22 @@ public class BlockChangerPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
-        this.getLogger().info("Starting BlockChanger plugin...");
-        new BlockChangerJoinListener(this);
-        this.getLogger().info("Plugin created by Maksymilian Sybicki");
+        this.getLogger().info(prefixTrue + "Starting BlockChanger plugin...");
         getConfig().options().copyDefaults(true);
         saveConfig();
+        loadConfig(this);
+        new BlockChangerJoinListener(this);
+        this.getLogger().info(prefixTrue + "Plugin created by Maksymilian Sybicki");
     }
 
     @Override
     public void onDisable() {
-        this.getLogger().info("Stopping BlockChanger plugin...");
+        this.getLogger().info(prefixFalse + "Stopping BlockChanger plugin...");
     }
 
-    private final String prefixTrue = ChatColor.AQUA + "[" + getConfig().getString("prefix") + "]" + ChatColor.GREEN + " ";
-    private final String prefixFalse = ChatColor.AQUA + "[" + getConfig().getString("prefix") + "]" + ChatColor.RED + " ";
+    private static final String prefix = ChatColor.AQUA + "[";
+    static String prefixTrue;
+    private String prefixFalse;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -55,8 +61,18 @@ public class BlockChangerPlugin extends JavaPlugin {
                         prefixTrue + "/stopblockchange: stops blocks changing\n" +
                         prefixTrue + "/blockchanger [info]: shows info");
                 return true;
+            } else if (args[0].equalsIgnoreCase("reload")) {
+                loadConfig(this);
+                sender.sendMessage(prefixTrue + "Successfully reloaded config.yml!");
             }
         }
         return true;
+    }
+
+    public void loadConfig(Plugin plugin) {
+        File cfile = new File(plugin.getDataFolder().getAbsolutePath() + "/config.yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(cfile);
+        prefixTrue = prefix + config.getString("prefix") + "]" + ChatColor.GREEN + " ";
+        prefixFalse = prefix + config.getString("prefix") + "]" + ChatColor.RED + " ";
     }
 }
